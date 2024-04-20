907c033678f29668502a8027d83509d5116804b3
Adam Musial <adammusial@wp.pl>
2024-04-20 18:46:05 +0200

    adding properties to globalstyle

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -12,9 +12,16 @@ export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0 auto;
         font-family: 'Inter', sans-serif;
-        background-color: ${({ theme }) => theme.colors.global.background};
+        font-size: 18px;
+        letter-spacing: 0.05em;
+        background: ${({ theme }) => theme.colors.global.background};
         color: ${({ theme }) => theme.colors.global.textSecondary};
         transition: background 0.3s;
         height: 2000px; // to see background
+        padding-bottom: 109px;
+
+        @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+            padding-bottom: 31px;
+        }
     }
 `;
