cf35bb7859233a6a138590c6499c3c6b5209904f
Adam Musial <adammusial@wp.pl>
2025-11-21 22:33:00 +0100

    skillset added

--- src/features/personalHomepage/PersonalHomepage/index.js
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -1,11 +1,16 @@
 import { ThemeSwitch } from "../../../common/ThemeSwitch";
 import { AboutMe } from "./AboutMe";
 import { Container } from "./styled";
-
+import { Skills } from "./Skills/index.js";
+import { skills, nextSkills } from "./skillsData";
 
 export const PersonalHomepage = () => (
     <Container>
         <ThemeSwitch />
         <AboutMe />
+        <main>
+            <Skills title={<>My skillset includes 🛠</>} skills={skills} />
+            <Skills title={<>What I want to learn next 🚀</>} skills={nextSkills} />
+        </main>
     </Container>
 );
